73fc56a0a92812592df697b932c9b228fc4e4ac3
Hemanth N <Hemanth N>
2016-09-21 01:44:01 +0530

    major changes

--- Source.cpp
+++ Source.cpp
@@ -15,10 +15,10 @@ int main()
  {
 	 
 	std::cout << "Using OpenCV " << CV_MAJOR_VERSION << "." << CV_MINOR_VERSION << "." << CV_SUBMINOR_VERSION << std::endl;
-cout<<"hello";
+
 
 	 //Mat image;
-	 //image = imread("lena.jpg", CV_LOAD_IMAGE_COLOR); // Read the file
+	// image = imread("lena.jpg", CV_LOAD_IMAGE_COLOR); // Read the file
 	 //
 		//  if (!image.data) // Check for invalid input
 		//  {
@@ -29,12 +29,15 @@ cout<<"hello";
 		//  namedWindow("Display window", WINDOW_AUTOSIZE);// Create a window for display.
 	 //imshow("Display window", image); // Show our image inside it.
 
-
+	 
 	  VideoCapture capture;
 	  
 
 	  IBGS *bgs;
 	  bgs = new FrameDifferenceBGS;
+
+	  IBGS *bgs1;
+	  bgs1 = new FrameDifferenceBGS;
 
 	  /* Blob Tracking Algorithm */
 	  Mat img_blob;
@@ -46,10 +49,26 @@ cout<<"hello";
 	  if (!capture.isOpened())  // check if we succeeded
 		  cout<<"err";
 
-
-	  while (1)
-	  {
+	  
+	 // 
+	// 
+Mat img_bkgmodel;
 			Mat frame;
+	  Mat img_mask;
+	  Mat blurred;
+	  int j = 0;
+	  int area;
+	  string str,count;
+	  str = "Image";
+	  int frameno = 0;
+	  
+	  vector<vector<Point> > contours;
+	  vector<Vec4i> hierarchy;
+	 // Mat drawing;
+	  while (1)
+	  { 
+		 // Mat z1, z2;
+		  
 		  capture>>frame;
 		  //frame = cvQueryFrame(capture);
 		  if (frame.empty())
@@ -59,22 +78,65 @@ cout<<"hello";
 		  }
 
 		  Mat img_input(frame);
-		  //imshow("Input", img_input);
+		  imshow("Input", img_input);
 
-		  Mat img_mask;
-		  Mat img_bkgmodel;
-		  bgs->process(img_input, img_mask, img_bkgmodel);
+		 // 
+		 // bgs->process(img_input, img_mask, img_bkgmodel);
 
-		 if(!img_mask.empty())
-		   imshow("Foreground", img_mask);
+		 ////if(!img_mask.empty())
+		 // // imshow("Foreground", img_mask);
 
+		 // if (!img_mask.empty())
+		 // {
+			//  // Perform blob tracking
+			//  blobTracking->process(img_input, img_mask, img_blob);
+		 // }
+		 // line(img_input, Point(0, 0), Point(500, 500), Scalar(100, 155, 30));
+		 // imshow("drawing", img_input);
+		  Mat A = img_input(Range(100, 130), Range(230,550));
+
+		//  Mat B(A);
+
+		  bgs1->process(A, img_mask, img_bkgmodel);
 		  if (!img_mask.empty())
 		  {
-			  // Perform blob tracking
-			  blobTracking->process(img_input, img_mask, img_blob);
+			  medianBlur(img_mask, blurred, 9);
+			 //imshow("Foreground", blurred);
+		 
+			 cv::findContours(blurred, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
+
+			
+			
+			 
+
+			 Mat drawing = Mat::zeros(blurred.size(), CV_8UC3);
+			  for (size_t i = 0; i<contours.size(); ++i)
+			  {
+				  //cv::drawContours(drawing, contours, i, Scalar(200, 0, 0), 1, 8, hierarchy, 0);
+				  Rect brect = boundingRect(contours[i]);
+				  area= brect.width*brect.height;
+				
+				  if (area>300 && frameno==0)
+				  {
+					  count = to_string(j);
+					  cout << str+count;
+					  imwrite(str+count+".jpg", A);
+					  rectangle(drawing, brect, Scalar(255, 0, 0));
+					  j++; frameno++;
+				  }
+				  else
+					  frameno>15?frameno=0:frameno++;
+
+				 
+			  }
+			  ////cv::drawContours(frame,contours,-1,cv::Scalar(0,0,255),2);
+			  cv::imshow("Frame", drawing);
+		  
+		  
+			  
 		  }
-		  line(img_input, Point(0, 0), Point(500, 500), Scalar(100, 155, 30));
-		 // imshow("drawing", img_input);
+		  //imshow("extract", B);
+		  
 
 
 
